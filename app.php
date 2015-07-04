aa0c7727cabe5306d8d6aee319bf00c49eb2bdb9
Twin Panichsombat <roofimon@gmail.com>
2015-07-04 19:59:17 +0700

    Raname param from a to pattern

--- app.php
+++ app.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-function captcha($a, $b, $c, $d) {
+function captcha($pattern, $b, $c, $d) {
     $e = array(
         1 => "One",
         2 => "Two",
@@ -18,11 +18,11 @@ function captcha($a, $b, $c, $d) {
         2 => "-",
         3 => "*",
     );
-    if(($a < 1 or $a > 2) or ($c < 1 or $c > 3)){
+    if(($pattern < 1 or $pattern > 2) or ($c < 1 or $c > 3)){
         return "You shouldn't do this to me :(" . "\n";
     }
 
-        if($a==1)
+        if($pattern==1)
         {
             if($c == 1){
                 $c = "+";
@@ -39,13 +39,13 @@ function captcha($a, $b, $c, $d) {
 
             $b = $e[$b];
         }
-        else if($a==2){
+        else if($pattern==2){
             if($c == 1){
                 $c = "+";
             }
 
             if($c == 3){
-                $c = "x";
+                $c = "*";
             }
 
             if($c == 2)
@@ -64,7 +64,10 @@ $app->get('/', function () {
     return "";
 });
 $app->get('/captcha', function () use ($app) {
-    return captcha($rand(1,2), rand(1,9), rand(1,3), rand(1,9));
+    $pattern = rand(1, 2);
+    $operand = rand(1,9);
+    $operator = rand(1, 3);
+    return captcha($pattern, $operand, $operator, $operand);
 });
 
 $app->run();
